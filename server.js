6c110497344ed0961f94b473fcbb419918d7ee03
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-02-10 22:17:29 -0100

    Added a HOST const to server.js

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server running on ${process.env.NODE_ENV === 'production' ? `https://Vef2-verk2.onrender.com` : `https://localhost:${PORT}`}`);
+const HOST = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
+
+app.listen(PORT, HOST, () => {
+	console.log(`Server running on ${process.env.NODE_ENV === 'production' 
+			? `https://Vef2-verk2.onrender.com`
+			: `https://localhost:${PORT}`}`);
 });
