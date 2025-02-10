7db172544d9bbc25041ff3e6e56ecd2b5f07500e
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-02-10 19:20:34 -0100

    Reconfigured to use DATABASE_UR

--- src/models/db.js
+++ src/models/db.js
@@ -6,11 +6,7 @@ dotenv.config();
 const { Client } = pg;
 
 const client = new Client({
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
+	connectionString: process.env.DATABASE_URL,
 	ssl: {
 		rejectUnauthorized: false,
 	},
